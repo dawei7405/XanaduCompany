6148af029d442e9f5c8f958c68be12cd6cded918
weirongsheng <872894588@qq.com>
2024-05-06 23:06:06 +0800

    docker部署配置修改

--- web/src/utils/request.ts
+++ web/src/utils/request.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
   timeout: 10000,
   // baseURL: '/api'
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'http://192.168.100.160:3000/api'
   // baseURL: 'bingjikeji-server:3000/api'
 })
