7ed70f32df813b7f3e586c10689b16908e4994d3
weirongsheng <872894588@qq.com>
2024-05-08 23:32:36 +0800

    docker部署配置修改

--- server/config/dbinfo.js
+++ server/config/dbinfo.js
@@ -6,7 +6,7 @@ const config = {
     // host: "localhost", // 主机地址
     port: "3306", // 端口号
     dialect: "mysql", //数据库类型，支持: 'mysql', 'sqlite', 'postgres', 'mssql'
-    // logging: true, // 是否启用日志
+    logging: true, // 是否启用日志
 }
 module.exports = config
 // sequelize-auto -h 'localhost' -d 'bingjikeji' -u 'root' -x '123456' -p 3307 -t 'aw_user'
--- web/src/utils/request.ts
+++ web/src/utils/request.ts
@@ -4,5 +4,5 @@ export default axios.create({
   timeout: 10000,
   // baseURL: '/api'
   baseURL: 'http://182.135.122.28:8890/api'
-  // baseURL: 'bingjikeji-server:8890/api'
+  // baseURL: 'http://localhost:8890/api'
 })
